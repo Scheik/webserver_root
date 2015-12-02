e34f3b9268fde8bacdb23e5b715b2c8b60f076ee
user <scheik.todeswache@googlemail.com>
2015-12-02 22:35:42 +0100

    UPADATE Webtools/base_controller.html and add ros.js

---
+++ Webtools/js/ros.js
@@ -0,0 +1,63 @@
+window.onload = function()
+{
+// Connecting to ROS
+// -----------------
+
+var ros = new ROSLIB.Ros({
+  url : 'ws://robotik.ddns.net:9090'
+});
+
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+  console.log('Connection to websocket server closed.');
+});
+
+// Subscribing to a Topic
+// ----------------------
+
+var listener_md49data = new ROSLIB.Topic({
+  ros : ros,
+  name : '/md49_data',
+  messageType : 'custom_messages/md49_data'
+});
+
+listener_md49data.subscribe(function(message) {
+  //console.log('Received message on ' + listener_md49data.name + ': ' +'speed_l:'+ message.current_l + ' speed_r:' + message.speed_r );
+  document.getElementById('speed_l').innerHTML=message.speed_l;
+  document.getElementById('speed_r').innerHTML=message.speed_r;
+  document.getElementById('current_l').innerHTML=message.current_l*10 + 'mA';
+  document.getElementById('current_r').innerHTML=message.current_r*10 + 'mA';
+  //document.getElementById('id_current_left').innerHTML=message.volts + ' V';
+  //listener_md49data.unsubscribe();
+});
+
+
+var listener_md49encoders = new ROSLIB.Topic({
+  ros : ros,
+  name : '/md49_encoders',
+  messageType : 'custom_messages/md49_encoders'
+});
+
+listener_md49encoders.subscribe(function(encoder_message) {
+  //console.log('Received message on ' + listener_md49encoders.name);
+  document.getElementById('encoder_l').innerHTML=encoder_message.encoder_l;
+  document.getElementById('l_byte_1').innerHTML=encoder_message.encoderbyte1l;
+  document.getElementById('l_byte_2').innerHTML=encoder_message.encoderbyte2l;
+  document.getElementById('l_byte_3').innerHTML=encoder_message.encoderbyte3l;
+  document.getElementById('l_byte_4').innerHTML=encoder_message.encoderbyte4l;
+  document.getElementById('encoder_r').innerHTML=encoder_message.encoder_r;
+  document.getElementById('r_byte_1').innerHTML=encoder_message.encoderbyte1r;
+  document.getElementById('r_byte_2').innerHTML=encoder_message.encoderbyte2r;
+  document.getElementById('r_byte_3').innerHTML=encoder_message.encoderbyte3r;
+  document.getElementById('r_byte_4').innerHTML=encoder_message.encoderbyte4r;
+  //listener_md49encoders.unsubscribe();
+});
+
+}
