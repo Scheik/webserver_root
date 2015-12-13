c0254c86db627da402accf4e7a6cecd9484901ae
user <scheik.todeswache@googlemail.com>
2015-12-13 15:33:08 +0100

    UPDATE Django Newspage: Using HTMLField now instead of TextField for bodytext of news

--- News/Newspage/migrations/0001_initial.py
+++ News/Newspage/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import tinymce.models
 
 
 class Migration(migrations.Migration):
@@ -16,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('author', models.CharField(max_length=40)),
                 ('title', models.CharField(max_length=200)),
-                ('bodytext', models.TextField()),
+                ('bodytext', tinymce.models.HTMLField()),
                 ('timestamp', models.DateTimeField()),
             ],
         ),
--- News/Newspage/models.py
+++ News/Newspage/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class posts(models.Model):
 
 	author = models.CharField(max_length = 40)
 	title = models.CharField(max_length = 200)
-	bodytext = models.TextField()
+	bodytext = HTMLField()
 	timestamp = models.DateTimeField()
 	
 	def __unicode__(self): #Python 3 is __str__
